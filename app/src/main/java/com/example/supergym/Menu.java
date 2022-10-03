6d4cf312c1a4ce6597bbc2f07cac5b6169c1d2fa
lsperez <lucasperez381@gmail.com>
2022-10-03 13:02:34 -0300

    Diseño de la Bottom Navigation Bar

--- app/src/main/java/com/example/supergym/Menu.java
+++ app/src/main/java/com/example/supergym/Menu.java
@@ -1,37 +1,66 @@
 package com.example.supergym;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Menu extends AppCompatActivity {
-Button rutinas,perfil,contacto,ejercicios;
-Usuario user;
+
+    InicioFragment inicioFragment = new InicioFragment();
+    ProgramaFragment programaFragment = new ProgramaFragment();
+    EntrenadorFragment entrenadorFragment = new EntrenadorFragment();
+    EjercicioFragment ejercicioFragment = new EjercicioFragment();
+    PerfilFragment perfilFragment = new PerfilFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        rutinas = findViewById(R.id.botonrutinas);
-        perfil = findViewById(R.id.botonperfil);
-        ejercicios = findViewById(R.id.botonejercicios);
-        contacto = findViewById(R.id.botoncontacto);
-        user = (Usuario) getIntent().getSerializableExtra("user");
+        BottomNavigationView navigationView = findViewById(R.id.bottomnavigationbar);
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        contacto.setOnClickListener(view ->{
-            Intent i = new Intent( Menu.this, Contacto.class);
-            i.putExtra("user", user);
-            startActivity(i);
-        });
-
-        perfil.setOnClickListener(view ->{
-            Intent i = new Intent( Menu.this, Perfil.class);
-            i.putExtra("user", user);
-            startActivity(i);
-        });
+        loadFragment(inicioFragment);
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.inicioFragment:
+                    loadFragment(inicioFragment);
+                    return true;
+                case R.id.programaFragment:
+                    loadFragment(programaFragment);
+                    return true;
+                case R.id.entrenadorFragment:
+                    loadFragment(entrenadorFragment);
+                    return true;
+                case R.id.ejercicioFragment:
+                    loadFragment(ejercicioFragment);
+                    return true;
+                case R.id.perfilFragment:
+                    loadFragment(perfilFragment);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.commit();
+    }
+
+
 }
